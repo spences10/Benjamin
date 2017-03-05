589a093a86fe9c3945eb841047a2dcb6b88bafe4
talonbragg <pvpgamecrash@gmail.com>
2017-03-05 17:33:00 +0000

    Change the logic of this

--- scripts/finance.js
+++ scripts/finance.js
@@ -1,5 +1,5 @@
 //Simple interest calculation
-document.getElementById('simpleInterestButton').addEventListener('click', function() {
+function simpleCalc() {
 		var sCalc = document.getElementById('princ').value * document.getElementById('percent').value * document.getElementById('time'); 
 		document.getElementById('answer'),innerHTML = parseInt(sCalc)
 });
