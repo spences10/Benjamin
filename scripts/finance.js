12e1dc5251d5e130524e9eb72f19c633f9cfea5a
talonbragg <pvpgamecrash@gmail.com>
2017-03-05 17:40:38 +0000

    Fix a logic error

--- scripts/finance.js
+++ scripts/finance.js
@@ -1,5 +1,5 @@
 //Simple interest calculation
 function simpleCalc() {
 		var sCalc = document.getElementById('princ').value * document.getElementById('percent').value * document.getElementById('time'); 
-		document.getElementById('answer'),innerHTML = parseInt(sCalc)
+		document.getElementById('answer'),innerHTML = parseInt(sCalc);
 });
