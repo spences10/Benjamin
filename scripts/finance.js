01e9c4120ab0c732fd163ce849b5ab83e349d077
talonbragg <pvpgamecrash@gmail.com>
2017-03-05 17:47:25 +0000

    Fix another error

--- scripts/finance.js
+++ scripts/finance.js
@@ -2,4 +2,4 @@
 function simpleCalc() {
 		var sCalc = document.getElementById('princ').value * document.getElementById('percent').value * document.getElementById('time'); 
 		document.getElementById('answer'),innerHTML = parseInt(sCalc);
-});
+}
