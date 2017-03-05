446485fd32c6febdb6f33af94a1c0656388a46c8
talonbragg <pvpgamecrash@gmail.com>
2017-03-05 19:52:59 +0000

    Change javascript a little bit

--- scripts/finance.js
+++ scripts/finance.js
@@ -1,5 +1,5 @@
 //Simple interest calculator
 function simpleCalc() {
 	var sCalc = document.getElementById('princ').value * document.getElementById('percent').value * document.getElementById('time').value; 
-	document.getElementById('answer').innerHTML = sCalc;
+	document.getElementById('answer').innerHTML = '$' + ' ' + sCalc;
 }	
