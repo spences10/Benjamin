e79b7c33e5923dccfa1d00c20102ebef9ca0d15d
talonbragg <pvpgamecrash@gmail.com>
2017-03-06 11:24:54 -0800

    Add profit calculator

--- scripts/finance.js
+++ scripts/finance.js
@@ -2,4 +2,8 @@
 function simpleCalc() {
 	var sCalc = document.getElementById('princ').value * document.getElementById('percent').value * document.getElementById('time').value; 
 	document.getElementById('answer').innerHTML = '$' + ' ' + sCalc;
+}
+function profitCalc() {
+	var pCalc = document.getElementById('moneymade').value - document.getElementById('moneyspent').value;
+	document.getElementById('profitAnswer').innerHTML = '$' + ' ' + pCalc;
 }	
