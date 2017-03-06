4b03222ca4a11b621a4b4a5c8dc0468538de549f
Scott Spence <spences10apps@gmail.com>
2017-03-06 14:38:51 +0000

    format simple calc

--- scripts/finance.js
+++ scripts/finance.js
@@ -3,3 +3,7 @@ function simpleCalc() {
 	var sCalc = document.getElementById('princ').value * document.getElementById('percent').value * document.getElementById('time').value; 
 	document.getElementById('answer').innerHTML = '$' + ' ' + sCalc;
 }	
+
+function formatPrice(cents) {
+  return `$${(cents / 100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+}
